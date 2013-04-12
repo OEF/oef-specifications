85ca396c6c53b7994449c8d9f6aecb14a9f74282
David Trémouilles <david.trem@gmail.com>
2013-04-12 10:56:32 +0300

    Create generate_demo-oef_file.py

---
+++ generate_demo-oef_file.py
@@ -0,0 +1,31 @@
+import h5py
+
+oef_file = h5py.File('demo.oef', 'w')
+
+grp = oef_file.create_group('experiment1')
+
+grp = oef_file.create_group('experiment2')
+grp.attrs['oef_data_type'] = 'tlp'
+
+grp = oef_file.create_group('experiment3')
+grp.attrs['oef_data_type'] = 'tlp'
+grp.attrs['oef_version'] = '0.1dev'
+
+grp = oef_file.create_group('experiment4')
+grp.attrs['oef_data_type'] = 'tlb'
+grp.attrs['oef_version'] = '0.1a1'
+
+grp = oef_file.create_group('experiment5')
+grp.attrs['oef_data_type'] = 'tlp'
+grp.attrs['oef_version'] = '0.1a2'
+
+grp = oef_file.create_group('experiment6')
+grp.attrs['oef_data_type'] = 'tlp'
+grp.attrs['oef_version'] = '0.1a1'
+
+grp = oef_file.create_group('experiment6/experiment7')
+grp.attrs['oef_data_type'] = 'tlp'
+grp.attrs['oef_version'] = '0.1a1'
+
+oef_file.flush()
+oef_file.close()
